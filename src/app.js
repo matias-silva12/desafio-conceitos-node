92fa4ca0f0ee12a28b9becb2675a75cca6b14a74
Matias Nilsson da Silva <matias.nds12@gmail.com>
2020-10-09 10:55:18 -0300

    Commiting first challenge of rocketseat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+
+  const results = title ? repositories.filter(project => project.title.includes(title)) : repositories;
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const project = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(project);
+
+  return response.status(201).json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+  const { id } = request.params;
+
+  const rep_index = repositories.findIndex(project => {
+    return project.id === id;
+  });
+
+  if (rep_index === -1)
+    return response.status(400).json({ error: "ID not found" });
+
+  const project = { id, title, url, techs, likes: repositories[rep_index].likes };
+  repositories[rep_index] = project;
+
+  return response.status(200).json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const rep_index = repositories.findIndex(project => {
+    return project.id === id;
+  });
+
+  if (rep_index === -1)
+    return response.status(400).send({ "error": "ID not found" });
+
+  repositories.splice(rep_index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const rep_index = repositories.findIndex(project => {
+    return project.id === id;
+  });
+
+  if (rep_index === -1)
+    return response.status(400).send({ "error": "ID not found" });
+
+  repositories[rep_index].likes += 1;
+
+  return response.json(repositories[rep_index]);
 });
 
 module.exports = app;
